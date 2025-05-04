2aaac6413ce7760ceca20ceeb6867fa6615ee6b4
Muhammed Jazim <muhammedjazim3423@gmail.com>
2025-05-04 07:33:22 +0530

    added create operation for adding file operations tracker to the db. added more paths to avoid tracking unnecessary files. added arc mutex for the connection pool to lock the thread while db execution

--- update_tracker/src/main.rs
+++ update_tracker/src/main.rs
@@ -2,11 +2,22 @@ use notify::{Error, Event, EventKind, RecursiveMode, Watcher};
 use std::path::Path;
 use std::env;
 use rusqlite::{params, Connection, Result as SqlResult};
+use std::sync::{Arc, Mutex};
+use notify::event::RenameMode;
 
 #[derive(Debug)]
 struct FileEvent {
     event_kind: String,
     path: String,
+    extension: String
+}
+
+fn get_file_extension(path: &str) -> String {
+    Path::new(path)
+        .extension()
+        .and_then(|ext| ext.to_str())
+        .unwrap_or("")
+        .to_string()
 }
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -17,36 +28,89 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 event_kind TEXT NOT NULL,
-                path TEXT NOT NULL
+                path TEXT NOT NULL,
+                extension TEXT NOT NULL
             )",
         [],
     )?;
 
-    let mut watcher = notify::recommended_watcher(|res: std::result::Result<Event, Error>| {
+    let conn = Arc::new(Mutex::new(conn));
+    let conn_clone = Arc::clone(&conn);
+
+    let mut watcher = notify::recommended_watcher(move |res: std::result::Result<Event, Error>| {
         match res {
             Ok(event) => {
                 if let Some(path_str) = event.paths.first().and_then(|p| p.to_str()) {
                     
                     let exclude_patterns = [
                         "\\AppData\\",
+                        "\\$Recycle.Bin",
+                        "\\System Volume Information",
+                        "\\.cache",
+                        "\\Temp\\",
+                        "\\tmp\\",
+                        "\\Windows\\",
+                        "\\ProgramData\\",  
+
                         "\\.git",
+                        "\\.svn",
+                        "\\.hg",
                         "\\node_modules",
                         "\\.vscode",
-                        "\\target",
+                        "\\.idea",
                         "\\.vs",
+                        "\\target",
                         "\\bin",
                         "\\obj",
                         "\\build",
-                        "\\dist"
+                        "\\dist",
+                        "\\out",
+                        "\\Debug",
+                        "\\Release",
+
+                        "\\.npm",
+                        "\\.yarn",
+                        "\\.cargo",
+                        "\\packages",
+
+                        "file_index.db",
+                        ".db-journal",
+                        ".db-shm",
+                        ".db-wal",
+                        ".sqlite-journal",
+                        ".sqlite-shm",
+                        ".sqlite-wal",
+
+                         "\\Cache\\",
+                        "\\CacheStorage\\"
                     ];
+
+                    let ignored_extensions = [
+                        "tmp", "temp", "swp", "swo", "bak", "~", "part",
+                        
+                        "lock",
+                        
+                        "log",
+                        
+                        "db", "sqlite", "db-journal", "db-shm", "db-wal",
+                        
+                        "pyc", "pyo", "obj", "o", "a", "lib", "so", "dll",
+                        
+                        "zip", "tar", "gz", "7z", "rar",
+                        
+                        "sys", "ini"
+                    ];
+
+                    let extension = get_file_extension(path_str);
                     
-                    if !exclude_patterns.iter().any(|&pattern| path_str.contains(pattern)) {
+                    if !exclude_patterns.iter().any(|&pattern| path_str.contains(pattern)) && !ignored_extensions.contains(&extension.as_str()){
                         let file_event = match event.kind {
                             EventKind::Create(_) => {
                                 println!("create event: {}", path_str);
                                 Some(FileEvent {
                                     event_kind: "create".to_string(),
                                     path: path_str.to_string(),
+                                    extension
                                 })
                             }
                             EventKind::Remove(_) => {
@@ -54,17 +118,49 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                                 Some(FileEvent {
                                     event_kind: "delete".to_string(),
                                     path: path_str.to_string(),
+                                    extension
                                 })
                             }
-                            EventKind::Modify(notify::event::ModifyKind::Name(_)) => {
-                                println!("modified name: {}", path_str);
-                                Some(FileEvent { 
-                                    event_kind: "rename".to_string(),
-                                    path: path_str.to_string()
-                                })
+                            EventKind::Modify(notify::event::ModifyKind::Name(rename_mode)) => {
+                                match rename_mode {
+                                    RenameMode::From => {
+                                        println!("rename from: {}", path_str);
+                                        Some(FileEvent { event_kind: "rename_from".to_string(), path: path_str.to_string(), extension })
+                                    }
+                                    RenameMode::To => {
+                                        println!("rename to: {}", path_str);
+                                        Some(FileEvent { event_kind: "rename_to".to_string(), path: path_str.to_string(), extension })
+                                    }
+                                    RenameMode::Both => {
+                                        println!("rename both: {}", path_str);
+                                        Some(FileEvent { event_kind: "rename_both".to_string(), path: path_str.to_string(), extension })
+                                    }
+                                    _ => {
+                                        println!("unspecified");
+                                        Some(FileEvent { 
+                                            event_kind: "rename".to_string(),
+                                            path: path_str.to_string(),
+                                            extension
+                                        })
+                                    }
+                                }
                             }
                             _ => None
                         };
+
+                        if let Some(event) = file_event {
+                            if let Ok(conn) = conn_clone.lock() {
+                                match conn.execute(
+                                    "INSERT INTO file_index (event_kind, path, extension) VALUES (?1, ?2, ?3)",
+                                    params![event.event_kind, event.path, event.extension],
+                                ) {
+                                    Ok(_) => println!("Event saved to database"),
+                                    Err(e) => eprintln!("Failed to save to database: {}", e),
+                                }
+                            } else {
+                                eprintln!("Failed to acquire database lock");
+                            }
+                        }
                     }
                 } else {
                     println!("else statement")
